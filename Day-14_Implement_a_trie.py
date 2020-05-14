7d31e3f198fe3112330f0f458f1732e3e100d1a6
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-05-14 22:52:23 +0530

    Added Day-14 Solution

---
+++ Day-14_Implement_a_trie.py
@@ -0,0 +1,76 @@
+'''
+    Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+Note:
+
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.
+
+
+'''
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head={}
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        curr=self.head
+        for i in word:
+            if i not in curr:
+                curr[i]={}
+            curr=curr[i]
+        curr['*']=True
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        curr=self.head
+        for i in word:
+            if i not in curr:
+                return False
+            else:
+                curr=curr[i]
+        if '*' in curr:
+            return True
+        else:
+            return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        curr=self.head
+        for i in prefix:
+            if i not in curr:
+                return False
+            else:
+                curr=curr[i]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
