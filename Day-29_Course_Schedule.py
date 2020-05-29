cfd020dd64aa35c9f4e723b9fa81c53e9eb6f29c
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-05-29 15:19:02 +0530

    Added Day-29 Solution

---
+++ Day-29_Course_Schedule.py
@@ -0,0 +1,54 @@
+'''
+	There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+ 
+
+Constraints:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+1 <= numCourses <= 10^5
+   Hide Hint #1  
+This problem is equivalent to finding if a cycle exists in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
+   Hide Hint #2  
+Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera explaining the basic concepts of Topological Sort.
+   Hide Hint #3  
+Topological sort could also be done via BFS.
+
+
+
+
+'''
+class Solution:
+  def canFinish(self, N: int, prereqs: List[List[int]]) -> bool:
+    graph = defaultdict(list)
+    for a,b in prereqs: graph[a].append(b)
+
+    status = defaultdict(int)
+    def dfs(i):
+        if status[i]: return status[i] == 1
+        status[i] = -1
+        if not all(map(dfs, graph.get(i,()))): return False
+        status[i] = 1
+        return True
+
+    return all(map(dfs, graph))
